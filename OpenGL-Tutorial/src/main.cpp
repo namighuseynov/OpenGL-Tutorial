6726ea419a0cb0544522508024d16a1bb937dbeb
namikguseinoff <namik.guseynov@inbox.ru>
2022-09-23 14:02:42 +0400

    added glm package

--- OpenGL-Tutorial/src/main.cpp
+++ OpenGL-Tutorial/src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+#include <string>
 
 void framebuffer_size_callback(GLFWwindow* window, int widht, int height);
 void processInput(GLFWwindow* window);
@@ -42,6 +47,8 @@ int main() {
 		glGenBuffers(1, &VBO);
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		std::string VShader_Src = "#version 330 core layout (location = 0) in vec3 aPos; void main() { gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); }";
+
 
 		//process input 
 		processInput(window);
