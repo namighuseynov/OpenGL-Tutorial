73a8e0bb8a8bc69dd16ec67dc0617fa7a0f3b502
namikguseinoff <namik.guseynov@inbox.ru>
2022-09-30 15:47:22 +0400

    update

--- OpenGL-Tutorial/src/main.cpp
+++ OpenGL-Tutorial/src/main.cpp
@@ -1,13 +1,9 @@
-#include <iostream>
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
-#include "Shader.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
 
-#include <fstream>
-#include <sstream>
-#include <streambuf>
-#include <string>
-#include <math.h>
+#include "Shader.h"
 
 
 void framebuffer_size_callback(GLFWwindow* window, int widht, int height);
@@ -19,7 +15,6 @@ int main() {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__ 
@@ -41,51 +36,91 @@ int main() {
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	//Our Shader
-	Shader simpleShader("assets/Shaders/vertex_shader.nvs", "assets/Shaders/fragment_shader.nfs");
+	Shader shader("assets/Shaders/vertex_shader.nvs", "assets/Shaders/fragment_shader.nfs");
 	
 
-	//VERTICES
-	float vertices[]{
-		-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-		0.0f, 0.5f, 0.0f,	0.0f, 1.0f, 0.0f,
-		0.5f, -0.5f, 0.0f,	0.0f, 0.0f, 1.0f
+	//Vertices, indices
+	float vertices[] {
+		0.5f,   0.5f, 0.0f,    1.0f, 0.0f, 0.0f,	0.0f, 0.0f,
+		0.5f,  -0.5f, 0.0f,	   0.0f, 1.0f, 0.0f,	0.0f, 1.0f,
+		-0.5f, -0.5f, 0.0f,    0.0f, 0.0f, 1.0f,    1.0f, 1.0f,
+		-0.5f,  0.5f, 0.0f,    1.0f, 1.0f, 0.0f,	1.0f, 0.0f
+	};
+	unsigned int indices[] = {  
+		0, 1, 3,  
+		1, 2, 3  
 	};
 
-	//VAO, VBO
+	//VAO, VBO, EBO
 
-	unsigned int VAO, VBO;
+	unsigned int VAO, VBO, EBO;
 	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glGenBuffers(1, &EBO);
 	glGenVertexArrays(1, &VAO);
+
 	glBindVertexArray(VAO);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float)*6, (void*)0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float)*8, (void*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (void*)(3*sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(3*sizeof(float)));
 	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	//Texture
+	unsigned int texture;
+	glGenTextures(1, &texture);
+
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("assets/Textures/box_texture.jpg", &width, &height, &nrChannels, 0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
 
 	while (!glfwWindowShouldClose(window)) {
-		//process input 
+		//Process Input
 		processInput(window);
 
-		//render
+		//Render
 		glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		//draw shapes
+		//Draw Shapes
+		shader.Use();
 		float time = glfwGetTime();
-		float PosX = (float)0.5f * sin(time);
-		simpleShader.Use();
-		simpleShader.SetFloat("PosX", PosX);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		float offsetX = 0.5f*sin(time);
+		shader.SetFloat("offsetX", offsetX);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-
-		//send new frame to window
+		//Send new frame to window
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 
 	}
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 
 	glfwTerminate();
 	return 0;
