0a7938139c102a021f018dde10a09b39464e50aa
namikguseinoff <namik.guseynov@inbox.ru>
2022-10-11 12:43:13 +0400

    Update

--- OpenGL-Tutorial/src/main.cpp
+++ OpenGL-Tutorial/src/main.cpp
@@ -8,10 +8,13 @@
 #include "stb/stb_image.h"
 
 #include "Shader.h"
+#include "Camera.h"
 
 
 void framebuffer_size_callback(GLFWwindow* window, int widht, int height);
 void processInput(GLFWwindow* window);
+
+void Draw(Shader* shader);
 
 const float WIDTH = 800.0f;
 const float HEIGHT = 600.0f;
@@ -24,9 +27,10 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-#ifdef __APPLE__ 
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
+//#ifdef __APPLE__ 
+//	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+//#endif
+
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Opengl Tutorial", NULL, NULL);
 	if (window == NULL) {
 		std::cout << "ERROR_GLFW: Could not create window." << std::endl;
@@ -34,19 +38,17 @@ int main() {
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		std::cout << "ERROR_GLAD: Failed to initialize GLAD " << std::endl;
 		return -1;
 	}
 
-	glViewport(0, 0, WIDTH, HEIGHT);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
 	//Our Shader
-	Shader shader("assets/Shaders/vertex_shader.nvs", "assets/Shaders/fragment_shader.nfs");
+	Shader* shader = new Shader("assets/Shaders/vertex_shader.nvs", "assets/Shaders/fragment_shader.nfs");
 	
 
-	//Vertices, indices
+	//Vertices
 	float vertices[] = {
 	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
@@ -90,25 +92,16 @@ int main() {
 	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
 	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
-	unsigned int indices[] = {  
-		0, 1, 3,  
-		1, 2, 3  
-	};
 
-	//VAO, VBO, EBO
-
-	unsigned int VAO, VBO, EBO;
+	//VAO, VBO
+	unsigned int VAO, VBO;
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
 	glGenVertexArrays(1, &VAO);
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float)*5, (void*)0);
@@ -133,24 +126,12 @@ int main() {
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
 	}
-	else
-	{
+	else {
 		std::cout << "Failed to load texture" << std::endl;
 	}
 	stbi_image_free(data);
 
-	glm::vec3 cubePositions[] = {
-		glm::vec3(0.0f,  0.0f,  -2.0f),
-		glm::vec3(2.0f,  5.0f, -15.0f),
-		glm::vec3(-1.5f, -2.2f, -2.5f),
-		glm::vec3(-3.8f, -2.0f, -12.3f),
-		glm::vec3(2.4f, -0.4f, -3.5f),
-		glm::vec3(-1.7f,  3.0f, -7.5f),
-		glm::vec3(1.3f, -2.0f, -2.5f),	
-		glm::vec3(1.5f,  2.0f, -2.5f),
-		glm::vec3(1.5f,  0.2f, -1.5f),
-		glm::vec3(-1.3f,  1.0f, -1.5f)
-	};
+	
 	
 	//Update
 	while (!glfwWindowShouldClose(window)) {
@@ -163,40 +144,16 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		//Draw Shapes
-		shader.Use();
-		glm::mat4 model = glm::mat4(1.0f);
-		glm::mat4 view = glm::mat4(1.0f);
-		glm::mat4 projection = glm::mat4(1.0f);
+		Draw(shader);
 
-		float speed = -1*glfwGetTime();
-		std::cout << speed << std::endl;
-
-
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, speed));
-		projection = glm::perspective(glm::radians(45.0f), (WIDTH / HEIGHT), 0.01f, 10000.0f);
-
-		shader.SetMat4("view", view);
-		shader.SetMat4("projection", projection);
-
-		glBindVertexArray(VAO);
-		for (int i = 0; i < 10; i++) {
-			model = glm::mat4(1.0f);
-			model = glm::translate(model, cubePositions[i]);
-			float angle = 20.0f *(i+1)*glfwGetTime();
-			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-			shader.SetMat4("model", model);
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-		}
 
 		//Send new frame to window
 		glfwSwapBuffers(window);
 		glfwPollEvents();
-
 	}
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
-
+	delete shader;
 	glfwTerminate();
 	return 0;
 }
@@ -206,7 +163,67 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 }
 
 void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_W) || glfwGetKey(window, GLFW_KEY_UP)) {
+		std::cout << "up" << std::endl;
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) || glfwGetKey(window, GLFW_KEY_DOWN)) {
+		std::cout << "down" << std::endl;
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) || glfwGetKey(window, GLFW_KEY_LEFT)) {
+		std::cout << "left" << std::endl;
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) || glfwGetKey(window, GLFW_KEY_RIGHT)) {
+		std::cout << "right" << std::endl;
+	}
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE)) {
 		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+void Draw(Shader* shader) {
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  -2.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+	shader->Use();
+	glm::mat4 model = glm::mat4(1.0f);
+	glm::mat4 view = glm::mat4(1.0f);
+	glm::mat4 projection = glm::mat4(1.0f);
+	float speed = -1 * glfwGetTime();
+
+
+	glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+	glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+	glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+	glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+	glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+	const float radius = 10.0f;
+	float camX = sin(glfwGetTime()) * radius;
+	float camZ = cos(glfwGetTime()) * radius;
+	view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+
+
+	//view = glm::translate(view, glm::vec3(0.0f, 0.0f, speed));
+	projection = glm::perspective(glm::radians(45.0f), (WIDTH / HEIGHT), 0.01f, 10000.0f);
+
+	shader->SetMat4("view", view);
+	shader->SetMat4("projection", projection);
+
+	for (int i = 0; i < 10; i++) {
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, cubePositions[i]);
+		float angle = 20.0f * (i + 1) * glfwGetTime();
+		model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+		shader->SetMat4("model", model);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 	}
 }
